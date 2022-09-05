abb48f2762f7b0bd8133caa2ef84da052f894772
Samuel Henrique <94930912+samuhkls@users.noreply.github.com>
2022-09-05 17:54:19 -0300

    Add files via upload

---
+++ SenhaAtendimento/ListaChamados/src/model/Variables_DAO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+public class Variables_DAO {
+    
+    public static int numLivre;
+    public static int numPref;
+    public static int numPreferencial[]=new int[5];
+    public static int numNormal[]=new int[5];
+    public static int j=0,j2=0,l=0,m=1,n=2,o=3,p=4;
+    public static int l2=0,m2=1,n2=2,o2=3,p2=4;
+    public static int numero;
+    
+    
+}
